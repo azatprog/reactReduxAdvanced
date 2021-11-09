37ab544ddcefb7fbc26bd4f107ae742630a0d119
Azat Nobatov <azatprog@gmail.com>
2021-11-09 16:38:32 +0500

    add redux toolkit, use multiple slices, add logic to the template

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,23 +1,31 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
+
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+    const dispatch = useDispatch();
+    const { item } = props;
 
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+    const addToCartHandler = () => {
+        dispatch(cartActions.addItemToCart(item))
+    };
+
+    return (
+        <li className={classes.item}>
+            <Card>
+                <header>
+                    <h3>{item.title}</h3>
+                    <div className={classes.price}>${item.price.toFixed(2)}</div>
+                </header>
+                <p>{item.description}</p>
+                <div className={classes.actions}>
+                    <button onClick={addToCartHandler}>Add to Cart</button>
+                </div>
+            </Card>
+        </li>
+    );
 };
 
 export default ProductItem;
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,18 +2,22 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+
+    const DUMMY_PRODUCTS = [
+        { id: 'p1', price: 6, quantity: 1, totalPrice: 6, title: 'My first book', description: 'My first book I ever wrote' },
+        { id: 'p2', price: 12, quantity: 1, totalPrice: 12, title: 'My second book', description: 'My second book I ever wrote' },
+    ];
+
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(product => (
+                    <ProductItem key={product.id} item={product} />
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
